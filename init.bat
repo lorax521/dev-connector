f6271a9e4afd0f78f5212cf145f94750ee3dadb6
Raines II <0070642225@FEMA.DHS.GOV>
2020-01-15 22:49:13 -0700

    react

--- init.bat
+++ init.bat
@@ -1,3 +1,6 @@
 npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
 REM dev dependencies
 npm i -D nodemon concurrently
+npx create-react-app client
+cd client
+npm i axios react-router-dom redux react-redux redux-thunk redux-devtools-extension moment react-moment
